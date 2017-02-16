167a4da61bf13f929d213ce96ea9deb8bed6690e
Howard Ho <howard036060006@gmail.com>
2017-02-16 15:48:59 +0800

    First Create

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const {mongoose} = require('./db/mongoose.js');
+const {Todo} = require('./models/todo.js');
+const {User} = require('./models/user.js');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+	var todo = new Todo({
+		text: req.body.text
+	});
+
+	todo.save().then((doc) => {
+		res.send(doc);
+	}, (e) => {
+		res.status(400).send(e);
+	})
+});
+
+app.get('/todos', (req,res) => {
+	Todo.find().then((todos) => {
+		res.send({todos});
+	}, (e) => {
+		res.send(e);
+	});
+});
+
+app.listen(3000, ()=>{
+	console.log('server start at port 3000');
+})
+
+module.exports = {
+	app
+}
